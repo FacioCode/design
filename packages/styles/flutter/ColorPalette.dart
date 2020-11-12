d08ae91a7bb908b2e9590dead559cddadb07376e
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-12 18:36:23 -0300

    fix(styles): add missing className for flutter generated file

---
+++ packages/styles/flutter/ColorPalette.dart
@@ -0,0 +1,23 @@
+
+//
+// ColorPalette.dart
+//
+// Do not edit directly
+// Generated on Thu, 12 Nov 2020 21:35:54 GMT
+//
+
+
+import 'dart:ui';
+
+class ColorPalette {
+  ColorPalette._();
+  
+    static const baseGrey20 = Color(0xFFEDEDEE);
+    static const baseGrey30 = Color(0xFFE2E3E4);
+    static const baseGrey40 = Color(0xFFD1D3D6);
+    static const baseGrey50 = Color(0xFFAFB4BB);
+    static const baseGrey60 = Color(0xFF828C9B);
+    static const baseGrey70 = Color(0xFF424D5C);
+    static const baseGrey80 = Color(0xFF303846);
+    static const baseGrey90 = Color(0xFF1F252E);
+}
