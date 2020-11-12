4a86b22674e6fe75e539e78cd22175d51b2db031
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-12 18:24:06 -0300

    build(styles): add flutter-generated file

---
+++ packages/styles/flutter/styles.dart
@@ -0,0 +1,23 @@
+
+//
+// styles.dart
+//
+// Do not edit directly
+// Generated on Thu, 12 Nov 2020 21:20:59 GMT
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+  
+    static const colorBaseGrey20 = Color(0xFFEDEDEE);
+    static const colorBaseGrey30 = Color(0xFFE2E3E4);
+    static const colorBaseGrey40 = Color(0xFFD1D3D6);
+    static const colorBaseGrey50 = Color(0xFFAFB4BB);
+    static const colorBaseGrey60 = Color(0xFF828C9B);
+    static const colorBaseGrey70 = Color(0xFF424D5C);
+    static const colorBaseGrey80 = Color(0xFF303846);
+    static const colorBaseGrey90 = Color(0xFF1F252E);
+}
