d376a44f65909953d36395003de07fbb5a68d298
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-13 17:22:44 -0300

    feat(styles): add web platform config

---
+++ packages/styles/src/platforms/web.js
@@ -0,0 +1,15 @@
+const web = {
+  "buildPath": "dist/",
+  "files": [
+    {
+      "destination": "index.css",
+      "format": "css/variables",
+    }, {
+      "destination": "index.js",
+      "format": "javascript/module",
+    },
+  ],
+  "transformGroup": "web",
+};
+
+module.exports = web;
