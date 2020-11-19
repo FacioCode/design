7c7419be9a1e851e1e58bd8470cd4723d58a7257
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-18 21:58:41 -0300

    style(styles): adopt 2 spaces instead of 4

--- packages/styles/src/templates/flutter/library_class.dart
+++ packages/styles/src/templates/flutter/library_class.dart
@@ -3,7 +3,7 @@ var props = _.sortBy(allProperties, item => item.attributes.category + item.name
 %>part of <%= this.packageName %>;
 
 class <%= this.className %> {
-    <%= this.className %>._();
+  <%= this.className %>._();
 
-    <%= _.map(props, function(prop) { return 'static const ' + prop.name + ' = ' + prop.value + ";" }).join('\n    ') %>
+  <%= _.map(props, function(prop) { return 'static const ' + prop.name + ' = ' + prop.value + ";" }).join('\n  ') %>
 }
