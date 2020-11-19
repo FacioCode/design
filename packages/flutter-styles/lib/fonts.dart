8eca80956128690c55aa56b93030f46bad568c80
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-19 00:43:39 -0300

    feat(flutter-styles): add font weight to fonts.dart

--- packages/flutter-styles/lib/fonts.dart
+++ packages/flutter-styles/lib/fonts.dart
@@ -4,4 +4,7 @@ class Fonts {
   Fonts._();
 
   static const familyBase = "Open Sans";
+  static const weightBold = "700";
+  static const weightExtraBold = "800";
+  static const weightRegular = "400";
 }
