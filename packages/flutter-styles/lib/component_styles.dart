88625766481ab45aecbb5acd4d969f631f543933
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-22 17:49:17 -0300

    feat(flutter-styles): add component styles classes

---
+++ packages/flutter-styles/lib/component_styles.dart
@@ -0,0 +1,278 @@
+part of facio_styles;
+
+class AppBarStyles {
+
+  /// height for root
+  static const appBarRootHeight = 896.00;
+
+  /// backgroundColor for colorDefault
+  static const appBarColorDefaultBackgroundColor = Color(0xFFFFFFFF);
+
+}
+
+class ButtonStyles {
+
+  /// borderRadius for root
+  static const buttonRootBorderRadius = 128.00;
+
+  /// borderWidth for contained
+  static const buttonContainedBorderWidth = 0.00;
+
+  /// color for label
+  static const buttonLabelColor = Color(0xFF303846);
+
+  /// Default button font size can vary depending on platform.
+  static const buttonLabelFontSize = 224.00;
+
+  /// height for sizeMedium
+  static const buttonSizeMediumHeight = 512.00;
+
+  /// label for sizeMedium
+  static const buttonSizeMediumLabelPaddingBottom = 64.00;
+
+  /// label for sizeMedium
+  static const buttonSizeMediumLabelPaddingTop = 64.00;
+
+  /// height for sizeLarge
+  static const buttonSizeLargeHeight = 768.00;
+
+  /// label for sizeLarge
+  static const buttonSizeLargeLabelPaddingBottom = 192.00;
+
+  /// label for sizeLarge
+  static const buttonSizeLargeLabelPaddingTop = 192.00;
+
+}
+
+class ChipStyles {
+
+  /// borderRadius for root
+  static const chipRootBorderRadius = 256.00;
+
+  /// borderWidth for root
+  static const chipRootBorderWidth = 16.00;
+
+}
+
+class InputStyles {
+
+  /// Font size is the same of headline, formerly headline.
+  static const inputMediumFontSize = 272.00;
+
+  /// Font size is the same of headline2, formerly title1.
+  static const inputLargeFontSize = [object Object];
+
+}
+
+class LabelStyles {
+
+  /// Font size is the same of subtitle1, formerly subhead.
+  static const labelLargeFontSize = 240.00;
+
+  /// Font size is the same of caption.
+  static const labelMediumFontSize = 192.00;
+
+}
+
+class TypographyStyles {
+
+  /// color for headline1
+  static const typographyHeadline1Color = Color(0xFF1F252E);
+
+  /// fontFamily for headline1
+  static const typographyHeadline1FontFamily = "Open Sans";
+
+  /// Extremely large text. It is the most prominent element in the interface.
+  static const typographyHeadline1FontSize = 544.00;
+
+  /// We use extra bold everytime we want to show monetary info.
+  static const typographyHeadline1FontWeight = "800";
+
+  /// letterSpacing for headline1
+  static const typographyHeadline1LetterSpacing = -8.00;
+
+  /// lineHeight for headline1
+  static const typographyHeadline1LineHeight = 640.00;
+
+  /// color for headline2
+  static const typographyHeadline2Color = Color(0xFF303846);
+
+  /// fontFamily for headline2
+  static const typographyHeadline2FontFamily = "Open Sans";
+
+  /// Very, very large text
+  static const typographyHeadline2FontSize = 448.00;
+
+  /// fontWeight for headline2
+  static const typographyHeadline2FontWeight = "700";
+
+  /// letterSpacing for headline2
+  static const typographyHeadline2LetterSpacing = -6.08;
+
+  /// lineHeight for headline2
+  static const typographyHeadline2LineHeight = 640.00;
+
+  /// color for headline3
+  static const typographyHeadline3Color = Color(0xFF303846);
+
+  /// fontFamily for headline3
+  static const typographyHeadline3FontFamily = "Open Sans";
+
+  /// Very large text
+  static const typographyHeadline3FontSize = 352.00;
+
+  /// fontWeight for headline3
+  static const typographyHeadline3FontWeight = "700";
+
+  /// letterSpacing for headline3
+  static const typographyHeadline3LetterSpacing = -4.16;
+
+  /// lineHeight for headline3
+  static const typographyHeadline3LineHeight = 512.00;
+
+  /// color for headline4
+  static const typographyHeadline4Color = Color(0xFF303846);
+
+  /// fontFamily for headline4
+  static const typographyHeadline4FontFamily = "Open Sans";
+
+  /// Large text
+  static const typographyHeadline4FontSize = 320.00;
+
+  /// fontWeight for headline4
+  static const typographyHeadline4FontWeight = "700";
+
+  /// letterSpacing for headline4
+  static const typographyHeadline4LetterSpacing = -7.20;
+
+  /// lineHeight for headline4
+  static const typographyHeadline4LineHeight = 384.00;
+
+  /// color for headline5
+  static const typographyHeadline5Color = Color(0xFF303846);
+
+  /// fontFamily for headline5
+  static const typographyHeadline5FontFamily = "Open Sans";
+
+  /// Recommended for large text in dialogs.
+  static const typographyHeadline5FontSize = 272.00;
+
+  /// fontWeight for headline5
+  static const typographyHeadline5FontWeight = "700";
+
+  /// letterSpacing for headline5
+  static const typographyHeadline5LetterSpacing = -6.88;
+
+  /// lineHeight for headline5
+  static const typographyHeadline5LineHeight = 384.00;
+
+  /// color for bodyText1
+  static const typographyBodyText1Color = Color(0xFF424D5C);
+
+  /// fontFamily for bodyText1
+  static const typographyBodyText1FontFamily = "Open Sans";
+
+  /// fontSize for bodyText1
+  static const typographyBodyText1FontSize = 272.00;
+
+  /// fontWeight for bodyText1
+  static const typographyBodyText1FontWeight = "400";
+
+  /// letterSpacing for bodyText1
+  static const typographyBodyText1LetterSpacing = -6.88;
+
+  /// lineHeight for bodyText1
+  static const typographyBodyText1LineHeight = 384.00;
+
+  /// color for subtitle1
+  static const typographySubtitle1Color = Color(0xFF303846);
+
+  /// fontFamily for subtitle1
+  static const typographySubtitle1FontFamily = "Open Sans";
+
+  /// A little smaller than headline5.
+  static const typographySubtitle1FontSize = 240.00;
+
+  /// fontWeight for subtitle1
+  static const typographySubtitle1FontWeight = "400";
+
+  /// letterSpacing for subtitle1
+  static const typographySubtitle1LetterSpacing = -3.68;
+
+  /// lineHeight for subtitle1
+  static const typographySubtitle1LineHeight = 384.00;
+
+  /// color for subtitle2
+  static const typographySubtitle2Color = Color(0xFF424D5C);
+
+  /// fontFamily for subtitle2
+  static const typographySubtitle2FontFamily = "Open Sans";
+
+  /// fontSize for subtitle2
+  static const typographySubtitle2FontSizeFontSize = 240.00;
+
+  /// fontWeight for subtitle2
+  static const typographySubtitle2FontWeight = "400";
+
+  /// letterSpacing for subtitle2
+  static const typographySubtitle2LetterSpacing = -3.68;
+
+  /// lineHeight for subtitle2
+  static const typographySubtitle2LineHeight = 384.00;
+
+  /// Button color is secondary (probably grey-80) by default
+  static const typographyButtonColor = Color(0xFF303846);
+
+  /// fontFamily for button
+  static const typographyButtonFontFamily = "Open Sans";
+
+  /// fontSize for button
+  static const typographyButtonFontSize = 224.00;
+
+  /// Button labels will have regular font weight by default but they can be bold.
+  static const typographyButtonFontWeight = "400";
+
+  /// letterSpacing for button
+  static const typographyButtonLetterSpacing = -2.40;
+
+  /// lineHeight for button
+  static const typographyButtonLineHeight = 384.00;
+
+  /// color for bodyText2
+  static const typographyBodyText2Color = Color(0xFF424D5C);
+
+  /// fontFamily for bodyText2
+  static const typographyBodyText2FontFamily = "Open Sans";
+
+  /// Low emphasis, useful for very small blocks
+  static const typographyBodyText2FontSize = 224.00;
+
+  /// fontWeight for bodyText2
+  static const typographyBodyText2FontWeight = "400";
+
+  /// letterSpacing for bodyText2
+  static const typographyBodyText2LetterSpacing = -2.40;
+
+  /// lineHeight for bodyText2
+  static const typographyBodyText2LineHeight = 384.00;
+
+  /// Caption color is secondary (probably grey-80) by default
+  static const typographyCaptionColor = Color(0xFF303846);
+
+  /// fontFamily for caption
+  static const typographyCaptionFontFamily = "Open Sans";
+
+  /// The smallest font size.
+  static const typographyCaptionFontSize = 192.00;
+
+  /// fontWeight for caption
+  static const typographyCaptionFontWeight = "400";
+
+  /// letterSpacing for caption
+  static const typographyCaptionLetterSpacing = 0.00;
+
+  /// lineHeight for caption
+  static const typographyCaptionLineHeight = 256.00;
+
+}
+
--- packages/styles/src/templates/flutter/package.dart
+++ packages/styles/src/templates/flutter/package.dart
@@ -3,5 +3,6 @@ library {{packageName}};
 import 'dart:ui';
 
 part 'color_palette.dart';
+part 'component_styles.dart';
 part 'sizes.dart';
 part 'text_theme.dart';
