71d3fae859be0ab3a3b07c0b18e616529fc5b594
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-22 19:03:49 -0300

    style(styles): make dart templates compliant with dart formatter

--- packages/styles/src/templates/flutter/component_styles.dart
+++ packages/styles/src/templates/flutter/component_styles.dart
@@ -1,11 +1,12 @@
 part of <%= packageName %>;
 <% _.forEach(props, (prop) => { const className = _.upperFirst(prop.key) + "Styles" %>
 class <%- className %> {
-
   <%- className %>._();
-  <% _.forEach(prop.value, (componentStyle) => {%>
-  <% console.log(componentStyle); if(componentStyle.comment) { %>/// <%= componentStyle.comment %><% } else { %>/// <%= componentStyle.attributes.subitem %> for <%= componentStyle.attributes.item %><% } %>
+<% _.forEach(prop.value, (componentStyle) => {%>
+<%
+if(componentStyle.comment) { %>  /// <%= componentStyle.comment %><%}
+else {
+%>  /// <%= componentStyle.attributes.subitem %> for <%= componentStyle.attributes.item %><% } %>
   static const <%= componentStyle.name %> = <%= componentStyle.value %>;
-<% }); %>
-}
+<% }); %>}
 <% }); %>
--- packages/styles/src/templates/flutter/text_theme.dart
+++ packages/styles/src/templates/flutter/text_theme.dart
@@ -1,7 +1,7 @@
 <%
 var props = _.chain(allProperties).groupBy("attributes.item").map((value, key) => ( { key, value } )).value();
 var textStyleMapper = style => style.attributes.subitem && `\n      ${style.attributes.subitem}: ${style.value || null}`;
-var textThemeMapper = prop => `\n  ${prop.key}: ${this.className}.${prop.key}`;
+var textThemeMapper = prop => `\n      ${prop.key}: ${this.className}.${prop.key}`;
 %>part of <%= this.packageName %>;
 
 /// # Text styles and theme for <%= this.packageName %>
@@ -15,19 +15,15 @@ var textThemeMapper = prop => `\n  ${prop.key}: ${this.className}.${prop.key}`;
 /// See also [TextTheme API](https://api.flutter.dev/flutter/material/TextTheme-class.html).
 ///
 class <%= this.className %> {
-
   <%= this.className %>._();
 <%= _.map(props, (prop) => {
 
   const mappedStyles = _.map(prop.value, textStyleMapper);
 
   return `
-  static const TextStyle ${prop.key} = TextStyle(${mappedStyles}
-  );
+  static const TextStyle ${prop.key} = TextStyle(${mappedStyles});
 `;
 
 }).join('') %>
-  static const TextTheme textTheme = TextTheme(${_.map(props, textThemeMapper)}
-  );
-
+  static const TextTheme textTheme = TextTheme(${_.map(props, textThemeMapper)});
 }
