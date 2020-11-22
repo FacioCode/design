d02c168574c33db43478f4f38dcf4b82a64b2b74
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-22 18:44:16 -0300

    fix(styles): add missing constructor at component styles template

--- packages/styles/src/templates/flutter/component_styles.dart
+++ packages/styles/src/templates/flutter/component_styles.dart
@@ -1,7 +1,9 @@
 part of <%= packageName %>;
-<% _.forEach(props, (prop) => { %>
-class <%- _.upperFirst(prop.key) %>Styles {
-<% _.forEach(prop.value, (componentStyle) => {%>
+<% _.forEach(props, (prop) => { const className = _.upperFirst(prop.key) + "Styles" %>
+class <%- className %> {
+
+  <%- className %>._();
+  <% _.forEach(prop.value, (componentStyle) => {%>
   <% console.log(componentStyle); if(componentStyle.comment) { %>/// <%= componentStyle.comment %><% } else { %>/// <%= componentStyle.attributes.subitem %> for <%= componentStyle.attributes.item %><% } %>
   static const <%= componentStyle.name %> = <%= componentStyle.value %>;
 <% }); %>
