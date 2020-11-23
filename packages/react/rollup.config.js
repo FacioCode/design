9d37e494841e0952b4c5e4b4797d641b382c38f4
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-23 11:34:12 -0300

    build(react): add rollup config

---
+++ packages/react/rollup.config.js
@@ -0,0 +1,10 @@
+import { browser, directories, displayName, module, source } from "./package.json";
+import config from "../../config/rollup/rollup.config";
+
+export default config({
+  browser,
+  directories,
+  displayName,
+  module,
+  source,
+});
