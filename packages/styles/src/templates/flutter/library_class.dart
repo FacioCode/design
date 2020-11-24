0ddb9105151bce8c5224212cd38060924ee4bfc9
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-24 17:25:13 -0300

    fix(styles): only add name and value prefixes if category is color

--- packages/styles/src/templates/flutter/library_class.dart
+++ packages/styles/src/templates/flutter/library_class.dart
@@ -1,9 +1,12 @@
 <%
 var props = _.sortBy(allProperties, item => item.attributes.category + item.name);
+var getNamePrefix = (category) => category === "color" ? "Color " : "";
+var getValuePrefix = (category) => category === "color" ? "const ": "";
+
 %>part of <%= this.packageName %>;
 
 class <%= this.className %> {
   <%= this.className %>._();
 
-  <%= _.map(props, function(prop) { return 'static const ' + prop.name + ' = ' + prop.value + ";" }).join('\n  ') %>
+  <%= _.map(props, ({attributes, name, value}) => `static const ${getNamePrefix(attributes.category)}${name} = ${getValuePrefix(attributes.category)}${value};`).join('\n  ') %>
 }
