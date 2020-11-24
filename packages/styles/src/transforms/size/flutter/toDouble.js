5eb316b633e3d6702562ec1ea7d0483f18dbc739
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-24 17:35:03 -0300

    fix(styles): convert typography and base sizes to double

--- packages/styles/src/transforms/size/flutter/toDouble.js
+++ packages/styles/src/transforms/size/flutter/toDouble.js
@@ -1,7 +1,9 @@
 const isBaseSize = require("../../../matchers/isBaseSize");
+const isFontSize = require("../../../matchers/isFontSize");
+const isLetterSpacing = require("../../../matchers/isLetterSpacing");
 
 const sizeFlutterToDouble = {
-  matcher: isBaseSize,
+  matcher: (prop) => isBaseSize(prop) || isFontSize(prop) || isLetterSpacing(prop),
   name: "size/flutter/toDouble",
   transformer(prop) {
     const fractionDigits = 2;
