0a2bd9f87a9b08276e7ded286c1c1feeb4092a80
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-24 16:52:44 -0300

    fix(flutter-styles): import missing material and painting

--- packages/flutter-styles/lib/facio_styles.dart
+++ packages/flutter-styles/lib/facio_styles.dart
@@ -1,6 +1,8 @@
 library facio_styles;
 
 import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 part 'color_palette.dart';
 part 'component_styles.dart';
