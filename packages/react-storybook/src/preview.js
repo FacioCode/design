6dff6c9242b45f71e52a6e4ecad8594e027b6a6c
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-24 22:04:12 -0300

    feat(react-storybook): add CssBaseline as decorator

---
+++ packages/react-storybook/src/preview.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { CssBaseline } from "../../react/src/CssBaseline";
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const decorators = [(Story) => <CssBaseline><Story /></CssBaseline>];
