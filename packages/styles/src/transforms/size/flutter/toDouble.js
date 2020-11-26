37665740311d8df9a19a9a81ac0dfaec906e29bc
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-26 17:43:04 -0300

    style(styles): simplify isLetterSpacing transformer function

--- packages/styles/src/transforms/size/flutter/toDouble.js
+++ packages/styles/src/transforms/size/flutter/toDouble.js
@@ -5,12 +5,8 @@ const isLetterSpacing = require("../../../matchers/isLetterSpacing");
 const sizeFlutterToDouble = {
   matcher: (prop) => isBaseSize(prop) || isFontSize(prop) || isLetterSpacing(prop),
   name: "size/flutter/toDouble",
-  transformer(prop) {
-    const fractionDigits = 2;
-
-
-    return (parseFloat(prop.value)).toFixed(fractionDigits);
-  },
+  // eslint-disable-next-line no-magic-numbers
+  transformer: ({ value }) => (parseFloat(value)).toFixed(2),
   type: "value",
 };
 
