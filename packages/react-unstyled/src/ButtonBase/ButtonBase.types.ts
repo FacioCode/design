7418d8879aef6d9da0513b4f54349cab59cf030b
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-26 16:59:58 -0300

    refactor(react): adopt UnstyledComponent type

--- packages/react-unstyled/src/ButtonBase/ButtonBase.types.ts
+++ packages/react-unstyled/src/ButtonBase/ButtonBase.types.ts
@@ -1,38 +1,5 @@
-import type { ElementType, HTMLAttributes, Ref } from "react";
+import type { UnstyledComponent } from "../UnstyledComponent";
 
+export type ClassKey = "root";
 export type Element = HTMLButtonElement | HTMLElement
-type Attributes = HTMLAttributes<Element>
-
-type ClassKey = |
-  "root";
-
-export type Props = {
-
-  /**
-   * @optional
-   * @type object
-   */
-  classes?: Record<ClassKey, Attributes["className"]>
-
-  /**
-   * @optional
-   * @type string
-   */
-  className?: Attributes["className"]
-
-  /**
-   * The component used for the root node. Either a `string` to use a HTML element or a component.
-   * ⚠️ Needs to be able to hold a ref.
-   *
-   * @default "component"
-   * @optional
-   * @type element
-   */
-  component?: ElementType
-
-  /**
-   * @optional
-   * @type ref
-   */
-  ref?: Ref<Element>
-}
+export type Props = UnstyledComponent<ClassKey, Element>
