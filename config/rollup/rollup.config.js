03794160f7323cbbb67418ade1694006dcc131b7
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-27 15:02:49 -0300

    build(rollup): add support to minifed UMD generation

--- config/rollup/rollup.config.js
+++ config/rollup/rollup.config.js
@@ -2,6 +2,7 @@ import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import path from "path";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 const manualChunks = (id) => path.parse(id).name;
@@ -20,34 +21,40 @@ export default ({ browser, displayName, globals = {}, source }) => [
     external: Object.keys(globals),
     input: source,
     manualChunks,
-    output: {
-      chunkFileNames: "[name].js",
-      dir: "dist",
-      format: "cjs",
-    },
+    output: [
+      {
+        chunkFileNames: "[name].js",
+        dir: "dist",
+        format: "cjs",
+      },
+      {
+        chunkFileNames: "[name].js",
+        dir: "dist/es",
+        format: "es",
+      },
+    ],
     plugins,
   },
   {
     external: Object.keys(globals),
     input: source,
-    manualChunks,
-    output: {
-      chunkFileNames: "[name].js",
-      dir: "dist/es",
-      format: "es",
-    },
-    plugins,
-  },
-  {
-    external: Object.keys(globals),
-    input: source,
-    output: {
-      file: browser,
-      format: "umd",
-      globals,
-      name: displayName,
-      sourcemap: true,
-    },
+    output: [
+      {
+        file: browser,
+        format: "umd",
+        globals,
+        name: displayName,
+        sourcemap: true,
+      },
+      {
+        file: "dist/umd/index.min.js",
+        format: "umd",
+        globals,
+        name: displayName,
+        plugins: [terser()],
+        sourcemap: true,
+      },
+    ],
     plugins,
   },
 ];
