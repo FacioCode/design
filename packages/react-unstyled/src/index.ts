58e8aef6e6b2c661bf6d0fcd99f01891677cf38e
Jimmy Andrade <contato@jimmyandrade.com>
2020-12-01 11:11:02 -0300

    feat(react-unstyled): make Paper component available

--- packages/react-unstyled/src/index.ts
+++ packages/react-unstyled/src/index.ts
@@ -2,4 +2,5 @@ export { ButtonBase, ButtonBaseElement, ButtonBaseProps } from "./ButtonBase";
 export { Card, CardElement, CardProps } from "./Card";
 export { FormLabel, FormLabelElement, FormLabelProps } from "./FormLabel";
 export { InputBase, InputBaseElement, InputBaseProps } from "./InputBase";
+export { Paper, PaperElement, PaperProps } from "./Paper";
 export { Typography, TypographyElement, TypographyProps } from "./Typography";
