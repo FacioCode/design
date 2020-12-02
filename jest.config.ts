c3af7c3429c0c97deb75be83e38888b9b9022a67
Jimmy Andrade <jimmy.andrade@facio.com>
2020-12-02 15:56:26 -0300

    test(jest): prevent mutated files to be tested

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,19 @@
+import { displayName } from "./package.json";
+
+const ignorePatterns = [
+  "/.stryker-tmp/",
+  "/node_modules/",
+];
+
 export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ignorePatterns,
+  displayName,
   projects: [
     "<rootDir>",
-    "<rootDir>/packages/*",
+    "<rootDir>/packages/react",
+    "<rootDir>/packages/react-storybook",
   ],
+  testPathIgnorePatterns: ignorePatterns,
 };
