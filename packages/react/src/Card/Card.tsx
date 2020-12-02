cc1e05291dcc6ef080f7fdc4b6c90c7c7622d109
Jimmy Andrade <jimmy.andrade@facio.com>
2020-12-02 15:26:37 -0300

    refactor(react): separate classes const from Card return

--- packages/react/src/Card/Card.tsx
+++ packages/react/src/Card/Card.tsx
@@ -10,9 +10,12 @@ import { useStyles } from "./Card.styles";
 
 export type CardProps = StyledProps<Props>;
 
-const renderer = (props: CardProps, ref: CardProps["ref"]) => (
-  <UnstyledCard {...props} component={Paper} ref={ref} classes={useStyles()} />
-);
+const renderer = (props: CardProps, ref: CardProps["ref"]) => {
+  const classes = useStyles();
+
+
+  return <UnstyledCard {...props} component={Paper} ref={ref} classes={classes} />;
+};
 
 export const Card = React.forwardRef<CardElement, CardProps>(renderer);
 Card.displayName = "Card";
