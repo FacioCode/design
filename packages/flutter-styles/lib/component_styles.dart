181b876c0056e8493bae6426328232299c6a1fa7
Jimmy Andrade <jimmy.andrade@facio.com>
2020-12-14 11:37:31 -0300

    feat(flutter-styles): refactor and add new component styles
    - Add `AlertStyles`;
    - Add colors for `Button`;
    - Add `InputStyles`;
    
    BREAKING CHANGE:  all component token constants were rename. `TypographyStyles` are rendundant with `TextThemes`, so `TypographyStyles` was removed too.

--- packages/flutter-styles/lib/component_styles.dart
+++ packages/flutter-styles/lib/component_styles.dart
@@ -1,315 +1,159 @@
 part of facio_styles;
 
+class AlertStyles {
+  AlertStyles._();
+
+  /// Border Radius for Alert
+  static const borderRadius = 8.00;
+
+  /// Background Color for Standard Info
+  static const standardInfoBackgroundColor = const Color(0xFFB3CCFF);
+}
+
 class AppBarStyles {
   AppBarStyles._();
 
-  /// backgroundColor for colorDefault
-  static const appBarColorDefaultBackgroundColor = const Color(0xFFFFFFFF);
+  /// Background Color for Color Default
+  static const colorDefaultBackgroundColor = const Color(0xFFF2F2F3);
 }
 
 class ButtonStyles {
   ButtonStyles._();
 
-  /// borderWidth for contained
-  static const buttonContainedBorderWidth = 0.00;
+  /// Border Width for Contained
+  static const containedBorderWidth = 0.00;
 
-  /// color for label
-  static const buttonLabelColor = const Color(0xFF303846);
+  /// Color for Label
+  static const labelColor = const Color(0xFF424D5C);
 
   /// Default button font size can vary depending on platform.
-  static const buttonLabelFontSize = 14.00;
-
-  /// borderRadius for sizeMedium
-  static const buttonSizeMediumBorderRadius = 24.00;
+  static const labelFontSize = 14.00;
 
   /// Medium button labels have semi bold font weight
-  static const buttonSizeMediumFontWeight = FontWeight.w600;
+  static const labelFontWeight = FontWeight.w600;
 
-  /// height for sizeMedium
-  static const buttonSizeMediumHeight = 32.00;
+  /// Color for Link
+  static const linkColor = const Color(0xFF068589);
 
-  /// label for sizeMedium
-  static const buttonSizeMediumLabelPaddingBottom = 4.00;
+  /// Border Color for Outlined
+  static const outlinedBorderColor = Color(0xFFE2E3E4);
 
-  /// label for sizeMedium
-  static const buttonSizeMediumLabelPaddingTop = 4.00;
+  /// Border Width for Outlined
+  static const outlinedBorderWidth = 1.00;
 
-  /// borderRadius for sizeLarge
-  static const buttonSizeLargeBorderRadius = 8.00;
+  /// Padding Bottom for Outlined Size Small
+  static const outlinedSizeSmallPaddingBottom = 2.00;
 
-  /// Large button labels have bold font weight
-  static const buttonSizeLargeFontWeight = FontWeight.w700;
+  /// Padding Top for Outlined Size Small
+  static const outlinedSizeSmallPaddingTop = 2.00;
 
-  /// height for sizeLarge
-  static const buttonSizeLargeHeight = 48.00;
+  /// Border Radius for Size Small
+  static const sizeSmallBorderRadius = 24.00;
 
-  /// label for sizeLarge
-  static const buttonSizeLargeLabelPaddingBottom = 12.00;
+  /// Height for Size Small
+  static const sizeSmallHeight = 32.00;
 
-  /// label for sizeLarge
-  static const buttonSizeLargeLabelPaddingTop = 12.00;
+  /// Padding Bottom for Size Small
+  static const sizeSmallPaddingBottom = 4.00;
+
+  /// All small buttons have left padding equivalent to 4 logical pixels
+  static const sizeSmallPaddingLeft = 12.00;
+
+  /// All small buttons have right padding equivalent to 4 logical pixels
+  static const sizeSmallPaddingRight = 12.00;
+
+  /// Padding Top for Size Small
+  static const sizeSmallPaddingTop = 4.00;
+
+  /// Border Radius for Size Medium
+  static const sizeMediumBorderRadius = 8.00;
+
+  /// Height for Size Medium
+  static const sizeMediumHeight = 48.00;
+
+  /// Padding Bottom for Size Medium
+  static const sizeMediumPaddingBottom = 12.00;
+
+  /// Padding Top for Size Medium
+  static const sizeMediumPaddingTop = 12.00;
+}
+
+class CardContentStyles {
+  CardContentStyles._();
+
+  /// Padding for Card Content
+  static const padding = 16.00;
 }
 
 class CardStyles {
   CardStyles._();
 
-  /// borderRadius for root
-  static const cardRootBorderRadius = 8.00;
+  /// Background Color for Card
+  static const backgroundColor = const Color(0xFFFFFFFF);
 
-  /// padding for root
-  static const cardRootPadding = 16.00;
+  /// Border Radius for Card
+  static const borderRadius = 8.00;
 
-  /// backgroundColor for default
-  static const cardDefaultBackgroundColor = const Color(0xFFFFFFFF);
-
-  /// color for default
-  static const cardDefaultColor = const Color(0xFF424D5C);
-
-  /// backgroundColor for info
-  static const cardInfoBackgroundColor = const Color(0xFFB3CCFF);
+  /// Color for Card
+  static const color = const Color(0xFF424D5C);
 }
 
 class ChipStyles {
   ChipStyles._();
 
-  /// borderRadius for root
-  static const chipRootBorderRadius = 24.00;
+  /// Border Radius for Chip
+  static const borderRadius = 24.00;
 
-  /// borderWidth for root
-  static const chipRootBorderWidth = 1.00;
+  /// Border Width for Chip
+  static const borderWidth = 1.00;
+}
+
+class InputBaseStyles {
+  InputBaseStyles._();
+
+  /// Color for Input Base
+  static const color = const Color(0xFF424D5C);
+
+  /// Font size is the same of headline, formerly headline.
+  static const mediumFontSize = 17.00;
+
+  /// Font size is the same of headline2, formerly title1.
+  static const largeFontSize = 28.00;
 }
 
 class InputStyles {
   InputStyles._();
 
-  /// color for root
-  static const inputRootColor = const Color(0xFF424D5C);
+  /// Border Bottom Color for Underline After
+  static const underlineAfterBorderBottomColor = Color(0xFF303846);
 
-  /// Font size is the same of headline, formerly headline.
-  static const inputMediumFontSize = 17.00;
-
-  /// Font size is the same of headline2, formerly title1.
-  static const inputLargeFontSize = 28.00;
+  /// Border Bottom Width for Underline After
+  static const underlineAfterBorderBottomWidth = 2.00;
 }
 
 class LabelStyles {
   LabelStyles._();
 
   /// Font size is the same of subtitle1, formerly subhead.
-  static const labelLargeFontSize = 15.00;
+  static const largeFontSize = 15.00;
 
   /// Font size is the same of caption.
-  static const labelMediumFontSize = 12.00;
+  static const mediumFontSize = 12.00;
 }
 
 class PaperStyles {
   PaperStyles._();
 
-  /// backgroundColor for root
-  static const paperRootBackgroundColor = const Color(0xFFFFFFFF);
+  /// Background Color for Paper
+  static const backgroundColor = const Color(0xFFFFFFFF);
 
-  /// color for root
-  static const paperRootColor = const Color(0xFF424D5C);
+  /// Color for Paper
+  static const color = const Color(0xFF424D5C);
 
-  /// borderRadius for rounded
-  static const paperRoundedBorderRadius = 8.00;
+  /// Border Radius for Rounded
+  static const roundedBorderRadius = 8.00;
 
-  /// borderRadius for square
-  static const paperSquareBorderRadius = 0.00;
+  /// Border Radius for Square
+  static const squareBorderRadius = 0.00;
 }
 
-class TypographyStyles {
-  TypographyStyles._();
-
-  /// color for headline1
-  static const typographyHeadline1Color = const Color(0xFF1F252E);
-
-  /// fontFamily for headline1
-  static const typographyHeadline1FontFamily = "Open Sans";
-
-  /// Extremely large text. It is the most prominent element in the interface.
-  static const typographyHeadline1FontSize = 34.00;
-
-  /// We use extra bold everytime we want to show monetary info.
-  static const typographyHeadline1FontWeight = FontWeight.w800;
-
-  /// letterSpacing for headline1
-  static const typographyHeadline1LetterSpacing = -0.50;
-
-  /// lineHeight for headline1
-  static const typographyHeadline1LineHeight = 1.17647058824;
-
-  /// color for headline2
-  static const typographyHeadline2Color = const Color(0xFF303846);
-
-  /// fontFamily for headline2
-  static const typographyHeadline2FontFamily = "Open Sans";
-
-  /// Very, very large text
-  static const typographyHeadline2FontSize = 28.00;
-
-  /// fontWeight for headline2
-  static const typographyHeadline2FontWeight = FontWeight.w700;
-
-  /// letterSpacing for headline2
-  static const typographyHeadline2LetterSpacing = -0.38;
-
-  /// lineHeight for headline2
-  static const typographyHeadline2LineHeight = 1.42857142857;
-
-  /// color for headline3
-  static const typographyHeadline3Color = const Color(0xFF303846);
-
-  /// fontFamily for headline3
-  static const typographyHeadline3FontFamily = "Open Sans";
-
-  /// Very large text
-  static const typographyHeadline3FontSize = 22.00;
-
-  /// fontWeight for headline3
-  static const typographyHeadline3FontWeight = FontWeight.w700;
-
-  /// letterSpacing for headline3
-  static const typographyHeadline3LetterSpacing = -0.26;
-
-  /// lineHeight for headline3
-  static const typographyHeadline3LineHeight = 1.45454545455;
-
-  /// color for headline4
-  static const typographyHeadline4Color = const Color(0xFF303846);
-
-  /// fontFamily for headline4
-  static const typographyHeadline4FontFamily = "Open Sans";
-
-  /// Large text
-  static const typographyHeadline4FontSize = 20.00;
-
-  /// fontWeight for headline4
-  static const typographyHeadline4FontWeight = FontWeight.w700;
-
-  /// letterSpacing for headline4
-  static const typographyHeadline4LetterSpacing = -0.45;
-
-  /// lineHeight for headline4
-  static const typographyHeadline4LineHeight = 1.2;
-
-  /// color for headline5
-  static const typographyHeadline5Color = const Color(0xFF303846);
-
-  /// fontFamily for headline5
-  static const typographyHeadline5FontFamily = "Open Sans";
-
-  /// Recommended for large text in dialogs.
-  static const typographyHeadline5FontSize = 17.00;
-
-  /// fontWeight for headline5
-  static const typographyHeadline5FontWeight = FontWeight.w700;
-
-  /// letterSpacing for headline5
-  static const typographyHeadline5LetterSpacing = -0.43;
-
-  /// lineHeight for headline5
-  static const typographyHeadline5LineHeight = 1.41176470588;
-
-  /// color for bodyText1
-  static const typographyBodyText1Color = const Color(0xFF424D5C);
-
-  /// fontFamily for bodyText1
-  static const typographyBodyText1FontFamily = "Open Sans";
-
-  /// fontSize for bodyText1
-  static const typographyBodyText1FontSize = 17.00;
-
-  /// fontWeight for bodyText1
-  static const typographyBodyText1FontWeight = FontWeight.w400;
-
-  /// letterSpacing for bodyText1
-  static const typographyBodyText1LetterSpacing = -0.43;
-
-  /// lineHeight for bodyText1
-  static const typographyBodyText1LineHeight = 1.41176470588;
-
-  /// color for subtitle1
-  static const typographySubtitle1Color = const Color(0xFF303846);
-
-  /// fontFamily for subtitle1
-  static const typographySubtitle1FontFamily = "Open Sans";
-
-  /// A little smaller than headline5.
-  static const typographySubtitle1FontSize = 15.00;
-
-  /// fontWeight for subtitle1
-  static const typographySubtitle1FontWeight = FontWeight.w700;
-
-  /// letterSpacing for subtitle1
-  static const typographySubtitle1LetterSpacing = -0.23;
-
-  /// lineHeight for subtitle1
-  static const typographySubtitle1LineHeight = 1.6;
-
-  /// color for subtitle2
-  static const typographySubtitle2Color = const Color(0xFF424D5C);
-
-  /// fontFamily for subtitle2
-  static const typographySubtitle2FontFamily = "Open Sans";
-
-  /// fontSize for subtitle2
-  static const typographySubtitle2FontSizeFontSize = 15.00;
-
-  /// fontWeight for subtitle2
-  static const typographySubtitle2FontWeight = FontWeight.w400;
-
-  /// letterSpacing for subtitle2
-  static const typographySubtitle2LetterSpacing = -0.23;
-
-  /// lineHeight for subtitle2
-  static const typographySubtitle2LineHeight = 1.6;
-
-  /// Button color is secondary (probably grey-80) by default
-  static const typographyButtonColor = const Color(0xFF303846);
-
-  /// fontFamily for button
-  static const typographyButtonFontFamily = "Open Sans";
-
-  /// fontSize for button
-  static const typographyButtonFontSize = 14.00;
-
-  /// letterSpacing for button
-  static const typographyButtonLetterSpacing = -0.15;
-
-  /// color for bodyText2
-  static const typographyBodyText2Color = const Color(0xFF424D5C);
-
-  /// fontFamily for bodyText2
-  static const typographyBodyText2FontFamily = "Open Sans";
-
-  /// Low emphasis, useful for very small blocks
-  static const typographyBodyText2FontSize = 14.00;
-
-  /// fontWeight for bodyText2
-  static const typographyBodyText2FontWeight = FontWeight.w400;
-
-  /// letterSpacing for bodyText2
-  static const typographyBodyText2LetterSpacing = -0.15;
-
-  /// lineHeight for bodyText2
-  static const typographyBodyText2LineHeight = 1.71428571429;
-
-  /// Caption color is secondary (probably grey-80) by default
-  static const typographyCaptionColor = const Color(0xFF303846);
-
-  /// fontFamily for caption
-  static const typographyCaptionFontFamily = "Open Sans";
-
-  /// The smallest font size.
-  static const typographyCaptionFontSize = 12.00;
-
-  /// fontWeight for caption
-  static const typographyCaptionFontWeight = FontWeight.w400;
-
-  /// letterSpacing for caption
-  static const typographyCaptionLetterSpacing = 0.00;
-
-  /// lineHeight for caption
-  static const typographyCaptionLineHeight = 1.33333333333;
-}
