56841340bd516190cc202f5d3423cda989d9d33a
Jimmy Andrade <jimmy.andrade@facio.com>
2020-12-15 20:55:04 -0300

    refactor(flutter-styles): separate styles for button variants
    BREAKING CHANGE: Many constants from `ButtonStyles` were moved to `ContainedButtonStyles`, `LinkButtonStyles` and `OutlinedButtonStyles`. `InputStyles` class was removed, since underline inputs will not be used anymore.

--- packages/flutter-styles/lib/component_styles.dart
+++ packages/flutter-styles/lib/component_styles.dart
@@ -20,35 +20,14 @@ class AppBarStyles {
 class ButtonStyles {
   ButtonStyles._();
 
-  /// Border Width for Contained
-  static const containedBorderWidth = 0.00;
-
-  /// Color for Label
-  static const labelColor = const Color(0xFF424D5C);
+  /// Border Radius for Full Width
+  static const fullWidthBorderRadius = 8.00;
 
   /// Default button font size can vary depending on platform.
   static const labelFontSize = 14.00;
 
   /// Medium button labels have semi bold font weight
   static const labelFontWeight = FontWeight.w600;
-
-  /// Color for Link
-  static const linkColor = const Color(0xFF068589);
-
-  /// Border Color for Outlined
-  static const outlinedBorderColor = Color(0xFFE2E3E4);
-
-  /// Border Width for Outlined
-  static const outlinedBorderWidth = 1.00;
-
-  /// Padding Bottom for Outlined Size Small
-  static const outlinedSizeSmallPaddingBottom = 2.00;
-
-  /// Padding Top for Outlined Size Small
-  static const outlinedSizeSmallPaddingTop = 2.00;
-
-  /// Border Radius for Size Small
-  static const sizeSmallBorderRadius = 24.00;
 
   /// Height for Size Small
   static const sizeSmallHeight = 32.00;
@@ -66,7 +45,7 @@ class ButtonStyles {
   static const sizeSmallPaddingTop = 4.00;
 
   /// Border Radius for Size Medium
-  static const sizeMediumBorderRadius = 8.00;
+  static const sizeMediumBorderRadius = 24.00;
 
   /// Height for Size Medium
   static const sizeMediumHeight = 48.00;
@@ -108,6 +87,109 @@ class ChipStyles {
   static const borderWidth = 1.00;
 }
 
+class ContainedButtonStyles {
+  ContainedButtonStyles._();
+
+  /// Border Width for Contained Button
+  static const borderWidth = 0.00;
+
+  /// Box Shadow for Hover
+  static const hoverBoxShadow = Color(0xFFD1D3D6);
+
+  /// Background Color for Brand
+  static const brandBackgroundColor = const Color(0xFF3FE1B6);
+
+  /// Color for Brand
+  static const brandColor = const Color(0xFF424D5C);
+
+  /// Background Color for Brand Hover
+  static const brandHoverBackgroundColor = const Color(0xFF3FE1B6);
+
+  /// Color for Brand Hover
+  static const brandHoverColor = const Color(0xFF424D5C);
+
+  /// Background Color for Brand Pressed
+  static const brandPressedBackgroundColor = const Color(0xFF2BC4A8);
+
+  /// Color for Brand Pressed
+  static const brandPressedColor = const Color(0xFF424D5C);
+
+  /// Background Color for Danger
+  static const dangerBackgroundColor = const Color(0xFFE13F6A);
+
+  /// Color for Danger
+  static const dangerColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Danger Hover
+  static const dangerHoverBackgroundColor = const Color(0xFFE13F6A);
+
+  /// Color for Danger Hover
+  static const dangerHoverColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Danger Pressed
+  static const dangerPressedBackgroundColor = const Color(0xFFC42B47);
+
+  /// Color for Danger Pressed
+  static const dangerPressedColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Default
+  static const defaultBackgroundColor = const Color(0xFF424D5C);
+
+  /// Color for Default
+  static const defaultColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Default Hover
+  static const defaultHoverBackgroundColor = const Color(0xFF424D5C);
+
+  /// Color for Default Hover
+  static const defaultHoverColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Default Pressed
+  static const defaultPressedBackgroundColor = const Color(0xFF303846);
+
+  /// Color for Default Pressed
+  static const defaultPressedColor = const Color(0xFFFFFFFF);
+
+  /// Border Radius for Size Small
+  static const sizeSmallBorderRadius = 24.00;
+
+  /// Background Color for Success
+  static const successBackgroundColor = const Color(0xFF068589);
+
+  /// Color for Success
+  static const successColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Success Hover
+  static const successHoverBackgroundColor = const Color(0xFF068589);
+
+  /// Color for Success Hover
+  static const successHoverColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Success Pressed
+  static const successPressedBackgroundColor = const Color(0xFF005C5E);
+
+  /// Color for Success Pressed
+  static const successPressedColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Warning
+  static const warningBackgroundColor = const Color(0xFFE1B63F);
+
+  /// Color for Warning
+  static const warningColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Warning Hover
+  static const warningHoverBackgroundColor = const Color(0xFFE1B63F);
+
+  /// Color for Warning Hover
+  static const warningHoverColor = const Color(0xFFFFFFFF);
+
+  /// Background Color for Warning Pressed
+  static const warningPressedBackgroundColor = const Color(0xFFC4A82B);
+
+  /// Color for Warning Pressed
+  static const warningPressedColor = const Color(0xFFFFFFFF);
+}
+
 class InputBaseStyles {
   InputBaseStyles._();
 
@@ -121,16 +203,6 @@ class InputBaseStyles {
   static const largeFontSize = 28.00;
 }
 
-class InputStyles {
-  InputStyles._();
-
-  /// Border Bottom Color for Underline After
-  static const underlineAfterBorderBottomColor = Color(0xFF303846);
-
-  /// Border Bottom Width for Underline After
-  static const underlineAfterBorderBottomWidth = 2.00;
-}
-
 class LabelStyles {
   LabelStyles._();
 
@@ -139,6 +211,47 @@ class LabelStyles {
 
   /// Font size is the same of caption.
   static const mediumFontSize = 12.00;
+}
+
+class LinkButtonStyles {
+  LinkButtonStyles._();
+
+  /// Color for Default
+  static const defaultColor = const Color(0xFF068589);
+
+  /// Border Radius for Size Small
+  static const sizeSmallBorderRadius = 8.00;
+}
+
+class OutlinedButtonStyles {
+  OutlinedButtonStyles._();
+
+  /// Border Width for Outlined Button
+  static const borderWidth = 1.00;
+
+  /// Border Color for Default
+  static const defaultBorderColor = Color(0xFFE2E3E4);
+
+  /// Color for Default
+  static const defaultColor = const Color(0xFF424D5C);
+
+  /// Border Color for Brand
+  static const brandBorderColor = Color(0xFFE2E3E4);
+
+  /// Color for Brand
+  static const brandColor = const Color(0xFF424D5C);
+
+  /// Border Color for Brand Hover
+  static const brandHoverBorderColor = Color(0xFF3FE1B6);
+
+  /// Border Radius for Size Small
+  static const sizeSmallBorderRadius = 24.00;
+
+  /// Padding Bottom for Size Small
+  static const sizeSmallPaddingBottom = 2.00;
+
+  /// Padding Top for Size Small
+  static const sizeSmallPaddingTop = 2.00;
 }
 
 class PaperStyles {
