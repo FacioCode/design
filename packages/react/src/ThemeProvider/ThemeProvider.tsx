388bb6f7e3b901ecf24f9d7f961bc277ac6c0244
Jimmy Andrade <jimmy.andrade@facio.com>
2020-12-15 23:56:16 -0300

    feat(react): add Facio theme as default one for ThemeProvider

--- packages/react/src/ThemeProvider/ThemeProvider.tsx
+++ packages/react/src/ThemeProvider/ThemeProvider.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { Component, ThemeProviderProps } from "./ThemeProvider.types";
 import { ThemeProvider as MaterialThemeProvider } from "@material-ui/core/styles";
+import { theme as defaultTheme } from "@faciocode/mui-themes";
 
 export const ThemeProvider : Component = (props: ThemeProviderProps) => {
-  const { children, theme, ...otherProps } = props;
+  const { children, theme = defaultTheme, ...otherProps } = props;
 
   return <MaterialThemeProvider theme={theme} {...otherProps}>{children}</MaterialThemeProvider>;
 };
