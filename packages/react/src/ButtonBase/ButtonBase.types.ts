1bf5c3564ff553331d1b017ce7657dfe28929718
Jimmy Andrade <jimmy.andrade@facio.com>
2020-12-15 21:08:16 -0300

    refactor(react): remove ButtonBase component
    BREAKING: `ButtonBase` React component is not available anymore. Please use `Button` component instead.

--- packages/react/src/ButtonBase/ButtonBase.types.ts
+++
@@ -1,5 +0,0 @@
-import type { Component } from "../Component";
-
-export type ClassKey = "root";
-export type Element = HTMLButtonElement | HTMLElement
-export type Props = Component<Element>
--- packages/react/src/ButtonBase/index.ts
+++
@@ -1,2 +0,0 @@
-export { ButtonBase } from "./ButtonBase";
-export type { Element as ButtonBaseElement, Props as ButtonBaseProps } from "./ButtonBase.types";
