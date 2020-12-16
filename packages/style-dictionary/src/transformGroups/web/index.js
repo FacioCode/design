27ea6be8c73c7610555b6eed81a5f76b109c545b
Jimmy Andrade <jimmy.andrade@facio.com>
2020-12-15 22:50:59 -0300

    feat(style-dictionary): add shadowCss transform to web transform group

--- packages/style-dictionary/src/transformGroups/web/index.js
+++ packages/style-dictionary/src/transformGroups/web/index.js
@@ -1,11 +1,17 @@
+const shadowCss = require("../../transforms/shadow/css");
+const sizeBasePx = require("../../transforms/size/web/px");
+const fontSizeToRem = require("../../transforms/size/fontSizeToRem");
+const lineHeightToEm = require("../../transforms/size/lineHeightToEm");
+
 const webBaseTransformGroup = {
   name: "webBase",
   transforms: [
     "attribute/cti",
     "color/css",
-    "size/web/px",
-    "size/fontSizeToRem",
-    "size/lineHeightToEm",
+    fontSizeToRem.name,
+    lineHeightToEm.name,
+    sizeBasePx.name,
+    shadowCss.name,
   ],
 };
 
