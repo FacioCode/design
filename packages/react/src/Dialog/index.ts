2392889a8d1dba596018b459de0207861f79837d
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-07 13:51:53 -0300

    feat(react): make `Dialog` component importable

---
+++ packages/react/src/Dialog/index.ts
@@ -0,0 +1,6 @@
+export { Dialog } from "./Dialog";
+export type { DialogProps } from "./Dialog.types";
+export * from "./DialogActions";
+export * from "./DialogContent";
+export * from "./DialogContentText";
+export * from "./DialogTitle";
