190f09b9c150868908209d72e03e34c302186059
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-07 13:56:45 -0300

    fix(style-dictionary): improve wrong syntax checking for values

--- packages/style-dictionary/src/matchers/isComponent/index.js
+++ packages/style-dictionary/src/matchers/isComponent/index.js
@@ -1,4 +1,10 @@
-const isComponent = ({ attributes }) => {
+const isComponent = (prop) => {
+  if (typeof prop.attributes === "undefined") {
+    console.error(`Can not get category from property with value ${prop.value}`);
+  }
+
+  const { attributes } = prop;
+
   const { category } = attributes;
 
 
