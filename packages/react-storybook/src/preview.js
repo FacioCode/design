c9595a0bf612edf543587c485519ab8c3daacb91
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-11 13:45:06 -0300

    style(react-storybook): update code style

--- packages/react-storybook/src/preview.js
+++ packages/react-storybook/src/preview.js
@@ -2,7 +2,9 @@ import * as React from "react";
 import { CssBaseline, ThemeProvider } from "@faciocode/react";
 import { theme } from "../../mui-themes/src";
 
-console.log(theme);
-
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const decorators = [(Story) => <ThemeProvider theme={theme}><CssBaseline><Story /></CssBaseline></ThemeProvider>];
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}><CssBaseline><Story /></CssBaseline></ThemeProvider>
+  ),
+];
