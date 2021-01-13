b312902447ac90148866e3d4bf09f46520c86070
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-13 18:16:45 -0300

    fix(react): prevent TypeScript error because of missing `css` prop on `TextField`

--- packages/react/src/TextField/TextField.types.ts
+++ packages/react/src/TextField/TextField.types.ts
@@ -1,10 +1,12 @@
 import type { TextFieldProps as MaterialTextFieldProps } from "@material-ui/core/TextField";
 
 export type MaterialTextFieldVariant = MaterialTextFieldProps["variant"];
-type OmittedProps = "hiddenLabel" | "size" | "variant";
+type OmittedProps = "css" | "hiddenLabel" | "size" | "variant";
 type TextFieldVariant = "filled" | "underline";
 
 export interface TextFieldProps extends Omit<MaterialTextFieldProps, OmittedProps> {
+
+  css?: never;
 
   /**
    * @default "filled"
