7a905670fcce31432a841aa6b2ed407e75d3eb58
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-20 17:01:43 -0300

    feat(styles): add size tokens
    `import { sizeTokens } from "@faciocode/styles"`

---
+++ packages/styles/src/sizeTokens.ts
@@ -0,0 +1,15 @@
+export const baseNone = "0px";
+export const baseOne = "1px";
+export const baseQuarter = "2px";
+export const baseHalf = "4px";
+export const baseSingle = "8px";
+export const baseSingleHalf = "12px"; // Commonly used for medium button vertical padding or dialog content text margin
+export const baseDouble = "16px"; // Twice the base value (8) = 16
+export const baseTriple = "24px"; // Commonly used for button label height
+export const baseQuadruple = "32px";
+export const baseFivefold = "40px";
+export const baseSixfold = "48px";
+export const baseSevenfold = "56px"; // Commonly used for app bar height
+export const baseEightfold = "64px"; // Commonly used for button min height
+export const baseThirtyOneFold = "248px"; // Button max width
+export const baseHalfPlusOne = "5px"; // Reserved for input padding for web (4+1)
