4c0445b194adf9a98ba4550660a7e2ed0b4d8b6b
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-20 17:01:53 -0300

    feat(styles): make size tokens importable

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -1,3 +1,4 @@
 export * as colorTokens from "./colorTokens";
 export * as componentTokens from "./componentTokens";
 export * as customProperties from "./customProperties";
+export * as sizeTokens from "./sizeTokens";
