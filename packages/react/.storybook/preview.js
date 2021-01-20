7a763a49d7487005c82d71a5bb7ba9e292036b4b
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-20 16:46:00 -0300

    feat(react storybook): add and configure backgrounds addon

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { CssBaseline, ThemeProvider } from "../src";
 import { theme } from "../../mui-themes/src";
+import { colorTokens, sizeTokens } from "@faciocode/styles";
 import lightTheme from "./themes/light";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -15,6 +16,39 @@ export const decorators = [
 ];
 
 export const parameters = {
+  backgrounds: {
+    default: "Default",
+    grid: {
+      cellSize: parseInt(sizeTokens.baseSingle),
+      cellAmount: parseInt(sizeTokens.baseHalf)
+    },
+    values: [
+      {
+        name: "Alert info",
+        value: colorTokens.backgroundAlertStandardInfo,
+      },
+      {
+        name: "Alert warning",
+        value: colorTokens.backgroundAlertStandardWarning,
+      },
+      {
+        name: "Brand",
+        value: colorTokens.backgroundBrand,
+      },
+      {
+        name: "Default",
+        value: colorTokens.backgroundDefault
+      },
+      {
+        name: "Paper",
+        value: colorTokens.backgroundPaper
+      },
+      {
+        name: "Selected",
+        value: colorTokens.backgroundSelected
+      }
+    ]
+  },
   docs: {
     theme: lightTheme
   }
