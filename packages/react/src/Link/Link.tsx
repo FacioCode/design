37ee55b32cc04e37202b20edfcb2cd2fc00c7b95
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-21 16:30:38 -0300

    feat(react): add support to color prop for `Link` component

--- packages/react/src/Link/Link.tsx
+++ packages/react/src/Link/Link.tsx
@@ -1,9 +1,36 @@
 import * as React from "react";
-import { LinkProps } from "./Link.types";
+import { ColorMapping, LinkProps, MaterialLinkColor } from "./Link.types";
 import { Link as MaterialLink } from "@material-ui/core";
+import clsx from "clsx";
+import { useStyles } from "./Link.styles";
 
 type Render = (props: LinkProps, ref: LinkProps["ref"]) => JSX.Element;
-const render : Render = (props, ref) => <MaterialLink {...props} ref={ref} />;
+const render : Render = (props, ref) => {
+  const { className, color = "default", ...otherProps } = props;
+
+  const colorMapping : ColorMapping = {
+    default: "inherit",
+    inherit: "inherit",
+    initial: "initial",
+  };
+  let mappedColor : MaterialLinkColor = "inherit";
+
+  if (typeof colorMapping[color] !== "undefined") {
+    mappedColor = colorMapping[color];
+  }
+
+  const { colorDefault } = useStyles();
+
+  const classNames = clsx({
+    [colorDefault]: color === "default",
+    // eslint-disable-next-line sort-keys
+    className,
+  });
+
+  return (
+    <MaterialLink {...otherProps} className={classNames} color={mappedColor} ref={ref} />
+  );
+};
 
 export const Link = React.forwardRef<unknown, LinkProps>(render);
 Link.displayName = "Link";
--- packages/react/src/Link/Link.types.ts
+++ packages/react/src/Link/Link.types.ts
@@ -1,3 +1,16 @@
 import { LinkProps as MaterialLinkProps } from "@material-ui/core/Link";
 
-export type LinkProps = Omit<MaterialLinkProps, "css">;
+type LinkColor = "default" | "initial" | "inherit";
+export type MaterialLinkColor = MaterialLinkProps["color"];
+type OmittedProps = "color" | "css" | "underline" | "variantMapping"
+export type ColorMapping = Record<LinkColor, MaterialLinkColor>;
+
+export interface LinkProps extends Omit<MaterialLinkProps, OmittedProps> {
+
+  /**
+   * @default "default"
+   * @optional
+   * @type "default" | "initial" | "inherit"
+   */
+  color?: LinkColor
+}
