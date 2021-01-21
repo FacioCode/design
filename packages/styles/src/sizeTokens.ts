283620f7fdf00df4ccd804a4502f3ce35222810a
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-21 17:40:17 -0300

    feat(styles): add base sizes for new button sizes

--- packages/styles/src/sizeTokens.ts
+++ packages/styles/src/sizeTokens.ts
@@ -1,12 +1,16 @@
 export const baseNone = "0px";
 export const baseOne = "1px";
 export const baseQuarter = "2px";
+export const baseFocus = "3px";
 export const baseHalf = "4px";
+export const baseHalfQuarter = "6px"; // Commonly used for small button vertical padding
 export const baseSingle = "8px";
 export const baseSingleHalf = "12px"; // Commonly used for medium button vertical padding or dialog content text margin
 export const baseDouble = "16px"; // Twice the base value (8) = 16
+export const baseDoubleQuarter = "18px"; // New border radius for small buttons
 export const baseTriple = "24px"; // Commonly used for button label height
-export const baseQuadruple = "32px";
+export const baseQuadruple = "32px"; // Formerly used for small button height.
+export const baseQuadrupleHalf = "36px"; // After some experiments, this is the new small button size
 export const baseFivefold = "40px";
 export const baseSixfold = "48px";
 export const baseSevenfold = "56px"; // Commonly used for app bar height
