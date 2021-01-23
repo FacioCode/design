f39246c32fb5bf36408f361671644dfe4b18079e
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-22 22:51:29 -0300

    feat(styles): add shadow tokens

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -1,4 +1,5 @@
 export * as colorTokens from "./colorTokens";
 export * as componentTokens from "./componentTokens";
 export * as customProperties from "./customProperties";
+export * as shadowTokens from "./shadowTokens";
 export * as sizeTokens from "./sizeTokens";
