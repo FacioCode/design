8b9fe2b4adff17b8fadde9d4936fb532719bbf85
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-23 20:09:29 -0300

    feat(react): add `elevation` property to `Paper` utility component

--- packages/react/src/Paper/Paper.stories.tsx
+++ packages/react/src/Paper/Paper.stories.tsx
@@ -5,10 +5,12 @@ import type { PaperProps } from "./Paper.types";
 
 export default {
   component: Paper,
-  title: "Components/Paper",
+  title: "Utilities/Paper",
 } as Meta;
 
-const Template : Story<PaperProps> = (props: PaperProps) => <Paper {...props} />;
+const Template : Story<PaperProps> = (props: PaperProps) => (
+  <Paper {...props}>{props.children}</Paper>
+);
 
 export const Playground : Story<PaperProps> = Template.bind({});
 Playground.args = {
@@ -19,4 +21,56 @@ export const Rounded : Story<PaperProps> = Template.bind({});
 Rounded.args = {
   children: "Rounded paper content",
   square: false,
+};
+
+export const Elevation0 : Story<PaperProps> = Template.bind({});
+Elevation0.args = {
+  ...Playground.args,
+  elevation: 0,
+};
+Elevation0.parameters = {
+  docs: {
+    description: {
+      story: "Most elements that use `Paper` as a base do not apply elevation.",
+    },
+  },
+};
+
+export const Elevation2 : Story<PaperProps> = Template.bind({});
+Elevation2.args = {
+  ...Playground.args,
+  elevation: 2,
+};
+Elevation2.parameters = {
+  docs: {
+    description: {
+      story: "Components like `ContainedButton` in hover state applies elevation `2`.",
+    },
+  },
+};
+
+export const Elevation4 : Story<PaperProps> = Template.bind({});
+Elevation4.args = {
+  ...Playground.args,
+  elevation: 4,
+};
+Elevation4.parameters = {
+  docs: {
+    description: {
+      story: "Floating alerts, like `SnackbarContent`, receive elevation `4`.",
+    },
+  },
+};
+
+export const Elevation6 : Story<PaperProps> = Template.bind({});
+Elevation6.args = {
+  ...Playground.args,
+  elevation: 6,
+};
+Elevation6.parameters = {
+  docs: {
+    description: {
+      story: "Components like the `BottomSheet` receive an elevation `6`.",
+    },
+  },
 };
--- packages/react/src/Paper/Paper.types.ts
+++ packages/react/src/Paper/Paper.types.ts
@@ -1,3 +1,22 @@
 import { PaperProps as MaterialPaperProps } from "@material-ui/core/Paper";
+import * as React from "react";
 
-export type PaperProps = Omit<MaterialPaperProps, "css" | "elevation">;
+// eslint-disable-next-line no-magic-numbers
+type PaperElevation = 0 | 2 | 4 | 6;
+
+export interface PaperProps extends Omit<MaterialPaperProps, "css" | "elevation"> {
+
+  /**
+   * The content of the component.
+   *
+   * @required
+   */
+  children: React.ReactNode;
+
+  /**
+   * @default 0
+   * @optional
+   * @type "0" | 2 | 4 | 6
+   */
+  elevation?: PaperElevation
+}
