68b71e36f39f70879d1064a5954cdcca05bbc2ef
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-23 20:08:23 -0300

    docs(react): add `FormLabel` stories

--- packages/react/src/FormLabel/FormLabel.stories.tsx
+++ packages/react/src/FormLabel/FormLabel.stories.tsx
@@ -12,7 +12,28 @@ const Template : Story<FormLabelProps> = (props: FormLabelProps) => (
   <FormLabel {...props} />
 );
 
+/**
+ * The font size of the label for the web is different from Flutter.
+ */
 export const Playground : Story<FormLabelProps> = Template.bind({});
 Playground.args = {
-  children: "Form label text",
+  children: "Nome social",
+};
+
+export const Disabled : Story<FormLabelProps> = Template.bind({});
+Disabled.args = {
+  ...Playground.args,
+  disabled: true,
+};
+
+export const Error : Story<FormLabelProps> = Template.bind({});
+Error.args = {
+  ...Playground.args,
+  error: true,
+};
+
+export const Focused : Story<FormLabelProps> = Template.bind({});
+Focused.args = {
+  ...Playground.args,
+  focused: true,
 };
