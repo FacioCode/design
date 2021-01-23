d66e266387ec3eee4785fbe45ac2b08f64f5d83e
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-23 20:03:52 -0300

    feat(mui-themes): add support to filled variant for `MuiAlert`

---
+++ packages/mui-themes/src/themeOptions/overrides/MuiAlert.ts
@@ -0,0 +1,15 @@
+/* eslint-disable sort-keys */
+
+import { customProperties } from "@faciocode/styles";
+
+export const MuiAlert = {
+  icon: customProperties.alertStyles.icon,
+  message: customProperties.alertStyles.message,
+  root: customProperties.alertStyles.root,
+  filledInfo: customProperties.alertStyles.filledInfo,
+  filledWarning: customProperties.alertStyles.filledWarning,
+  standardInfo: customProperties.alertStyles.standardInfo,
+  standardWarning: customProperties.alertStyles.standardWarning,
+};
+
+export default MuiAlert;
