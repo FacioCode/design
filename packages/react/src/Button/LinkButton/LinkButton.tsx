b6d54b82cfc464e2f6119208c95d14fc07067cdc
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-23 20:08:08 -0300

    feat(react): add support to `color` variant for `LinkButton`

--- packages/react/src/Button/LinkButton/LinkButton.tsx
+++ packages/react/src/Button/LinkButton/LinkButton.tsx
@@ -4,11 +4,13 @@ import { Button as MaterialButton } from "@material-ui/core";
 
 type Render = (props: LinkButtonProps, ref: LinkButtonProps["ref"]) => JSX.Element;
 
-const render : Render = (props, ref) => <MaterialButton
-  ref={ref}
-  variant={"text"}
-  {...props}
-/>;
+const render : Render = (props, ref) => (
+  <MaterialButton
+    ref={ref}
+    variant={"text"}
+    {...props}
+  />
+);
 
 export const LinkButton = React.forwardRef<unknown, LinkButtonProps>(render);
 LinkButton.displayName = "LinkButton";
--- packages/react/src/Button/LinkButton/LinkButton.types.ts
+++ packages/react/src/Button/LinkButton/LinkButton.types.ts
@@ -1,3 +1,14 @@
 import { ButtonProps } from "../Button.types";
+import { LinkColor } from "../../Link";
 
-export type LinkButtonProps = ButtonProps
+export interface LinkButtonProps extends ButtonProps {
+
+  /**
+   * You can set `Link` color to `inherit` if you need to use it inside a Snackbar.
+   *
+   * @default "default"
+   * @optional
+   * @type "default" | "inherit"
+   */
+  color?: LinkColor
+}
