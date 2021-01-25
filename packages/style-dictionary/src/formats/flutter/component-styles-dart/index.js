28e10444dac879bbe04eaea0e020cdf7a0603238
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-25 14:59:03 -0300

    refactor(style-dictionary): code refactoring for flutter component styles formatter

--- packages/style-dictionary/src/formats/flutter/component-styles-dart/index.js
+++ packages/style-dictionary/src/formats/flutter/component-styles-dart/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires,no-sync */
 const fs = require("fs");
-// eslint-disable-next-line id-length
 const { template } = require("lodash");
 const getAllPropertiesByAttributeType = require("../../../utils/getAllPropertiesByAttributeType");
-const templateFile = fs.readFileSync("./src/templates/flutter/component_styles");
+
 const flutterComponentStylesFormatter = (dictionary, options) => {
   const { allProperties } = dictionary;
   const { packageName } = options.metadata;
 
+  const templateFile = fs.readFileSync("./src/templates/flutter/component_styles");
   const props = getAllPropertiesByAttributeType(allProperties);
   const templateContent = templateFile.toString();
 
