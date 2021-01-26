b0886a735eb16c51619ebca29d03802d54bf88c3
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-26 15:30:56 -0300

    feat(flutter): move existing icons to `FacioSvgIcons`, add new ones
    BREAKING CHANGE: all existing icons from `FDSIcons` were moved `FacioSvgIcons`. Please find and replace it in your application.

---
+++ packages/flutter/lib/src/generated/svg_icons.dart
@@ -0,0 +1,194 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_import
+
+import 'package:flutter_svg/svg.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class FacioSvgIcons {
+  static final String packageName = 'facio_design_system';
+  /// Icon For App
+  static final app = SvgPicture.asset(
+    "assets/icons/app.svg",
+    package: packageName
+  );
+  /// Icon For Approval
+  static final approval = SvgPicture.asset(
+    "assets/icons/approval.svg",
+    package: packageName
+  );
+  /// Icon For Arrow Right
+  static final arrowRight = SvgPicture.asset(
+    "assets/icons/arrow-right.svg",
+    package: packageName
+  );
+  /// Icon For Arrow Up
+  static final arrowUp = SvgPicture.asset(
+    "assets/icons/arrow-up.svg",
+    package: packageName
+  );
+  /// Icon For Big Pig
+  static final bigPig = SvgPicture.asset(
+    "assets/icons/big-pig.svg",
+    package: packageName
+  );
+  /// Icon For Calendar
+  static final calendar = SvgPicture.asset(
+    "assets/icons/calendar.svg",
+    package: packageName
+  );
+  /// Icon For Close
+  static final close = SvgPicture.asset(
+    "assets/icons/close.svg",
+    package: packageName
+  );
+  /// Icon For Contract
+  static final contract = SvgPicture.asset(
+    "assets/icons/contract.svg",
+    package: packageName
+  );
+  /// Antecipacao De Salario
+  static final deposit = SvgPicture.asset(
+    "assets/icons/deposit.svg",
+    package: packageName
+  );
+  /// Icon For Double
+  static final double = SvgPicture.asset(
+    "assets/icons/double.svg",
+    package: packageName
+  );
+  /// Ocultar Saldo
+  static final eyeClosed = SvgPicture.asset(
+    "assets/icons/eye-closed.svg",
+    package: packageName
+  );
+  /// Exibir Saldo
+  static final eyeOpen = SvgPicture.asset(
+    "assets/icons/eye-open.svg",
+    package: packageName
+  );
+  /// Icon For Forbidden
+  static final forbidden = SvgPicture.asset(
+    "assets/icons/forbidden.svg",
+    package: packageName
+  );
+  /// Icon For Free
+  static final free = SvgPicture.asset(
+    "assets/icons/free.svg",
+    package: packageName
+  );
+  /// Icon For Gear
+  static final gear = SvgPicture.asset(
+    "assets/icons/gear.svg",
+    package: packageName
+  );
+  /// Educacao Financeira
+  static final growth = SvgPicture.asset(
+    "assets/icons/growth.svg",
+    package: packageName
+  );
+  /// Icon For Health
+  static final health = SvgPicture.asset(
+    "assets/icons/health.svg",
+    package: packageName
+  );
+  /// Icon For Hourglass
+  static final hourglass = SvgPicture.asset(
+    "assets/icons/hourglass.svg",
+    package: packageName
+  );
+  /// Icon For Id
+  static final id = SvgPicture.asset(
+    "assets/icons/id.svg",
+    package: packageName
+  );
+  /// Icon For Keyboard Arrow Down
+  static final keyboardArrowDown = SvgPicture.asset(
+    "assets/icons/keyboard-arrow-down.svg",
+    package: packageName
+  );
+  /// Icon For Keyboard Arrow Left
+  static final keyboardArrowLeft = SvgPicture.asset(
+    "assets/icons/keyboard-arrow-left.svg",
+    package: packageName
+  );
+  /// Icon For Keyboard Arrow Right
+  static final keyboardArrowRight = SvgPicture.asset(
+    "assets/icons/keyboard-arrow-right.svg",
+    package: packageName
+  );
+  /// Icon For Map
+  static final map = SvgPicture.asset(
+    "assets/icons/map.svg",
+    package: packageName
+  );
+  /// Icon For Megaphone
+  static final megaphone = SvgPicture.asset(
+    "assets/icons/megaphone.svg",
+    package: packageName
+  );
+  /// Icon For Payed
+  static final payed = SvgPicture.asset(
+    "assets/icons/payed.svg",
+    package: packageName
+  );
+  /// Poupe Facio
+  static final pig = SvgPicture.asset(
+    "assets/icons/pig.svg",
+    package: packageName
+  );
+  /// Icon For Play
+  static final play = SvgPicture.asset(
+    "assets/icons/play.svg",
+    package: packageName
+  );
+  /// Credito Consignado
+  static final received = SvgPicture.asset(
+    "assets/icons/received.svg",
+    package: packageName
+  );
+  /// Icon For Safe
+  static final safe = SvgPicture.asset(
+    "assets/icons/safe.svg",
+    package: packageName
+  );
+  /// Icon For Search
+  static final search = SvgPicture.asset(
+    "assets/icons/search.svg",
+    package: packageName
+  );
+  /// Icon For Taxes
+  static final taxes = SvgPicture.asset(
+    "assets/icons/taxes.svg",
+    package: packageName
+  );
+  /// Icon For Thunderbolt
+  static final thunderbolt = SvgPicture.asset(
+    "assets/icons/thunderbolt.svg",
+    package: packageName
+  );
+  /// Icon For Video
+  static final video = SvgPicture.asset(
+    "assets/icons/video.svg",
+    package: packageName
+  );
+  /// Icon For Waiting
+  static final waiting = SvgPicture.asset(
+    "assets/icons/waiting.svg",
+    package: packageName
+  );
+  /// Icon For Wallet
+  static final wallet = SvgPicture.asset(
+    "assets/icons/wallet.svg",
+    package: packageName
+  );
+  /// Icon For Warning
+  static final warning = SvgPicture.asset(
+    "assets/icons/warning.svg",
+    package: packageName
+  );
+  /// Icon For Whatsapp
+  static final whatsapp = SvgPicture.asset(
+    "assets/icons/whatsapp.svg",
+    package: packageName
+  );
+}
