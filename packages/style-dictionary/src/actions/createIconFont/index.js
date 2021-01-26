f32158af4c0f279d2dc5cae3eb0d7e382df4b07d
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-26 16:09:38 -0300

    refactor(style-dictionary): refactor icon font creation action

--- packages/style-dictionary/src/actions/createIconFont/index.js
+++ packages/style-dictionary/src/actions/createIconFont/index.js
@@ -2,46 +2,48 @@ const fs = require("fs");
 const webfont = require("webfont").default;
 
 const formats = ["eot", "svg", "ttf", "woff", "woff2"];
-const getFontPath = ({ buildPath, format }) => `${buildPath}assets/fonts/FacioIcons.${format}`;
-
-const doCallback = (dictionary, { buildPath }) => {
-  const webfontConfig = {
-    files: "assets/icons/*.svg",
-    fontName: "FacioIcons",
-  };
-
-  webfont(webfontConfig).then((result) => {
-    formats.forEach((format) => {
-      const path = getFontPath({ buildPath, format });
-
-      fs.writeFile(path, result[format], () => {
-        // eslint-disable-next-line no-console
-        console.log(`✔︎  ${path} created`);
-      });
-    });
-
-    return result;
-  }).
-    catch((error) => {
-      throw error;
-    });
-};
-
-const undoCallback = (dictionary, { buildPath }) => {
-  formats.forEach((format) => {
-    const path = getFontPath({ buildPath, format });
-
-    fs.unlink(path, () => {
-      // eslint-disable-next-line no-console
-      console.log(`${path} removed`);
-    });
-  });
-};
 
 const createIconFontAction = {
-  do: doCallback,
+  do: (dictionary, options) => {
+    const { buildPath } = options;
+
+    const webfontConfig = {
+      files: "assets/icons/*.svg",
+      fontName: "FacioIcons",
+    };
+
+    webfont(webfontConfig).then((result) => {
+      const getFontPath = ({ format }) => `${buildPath}assets/fonts/FacioIcons.${format}`;
+
+      console.log("\nwebfont");
+
+      formats.forEach((format) => {
+        const path = getFontPath({ format });
+
+        fs.writeFile(path, result[format], () => {
+          // eslint-disable-next-line no-console
+          console.log(`\x1b[32m✔\x1b[0m︎  ${path}`);
+        });
+      });
+
+      return result;
+    }).
+      catch((error) => {
+        throw error;
+      });
+  },
   name: "create_icon_font",
-  undo: undoCallback,
+  undo: (dictionary, { buildPath }) => {
+    const getFontPath = ({ format }) => `${buildPath}assets/fonts/FacioIcons.${format}`;
+
+    formats.forEach((format) => {
+      const path = getFontPath({ format });
+
+      fs.unlink(path, () => {
+        Function.prototype();
+      });
+    });
+  },
 };
 
 module.exports = createIconFontAction;
