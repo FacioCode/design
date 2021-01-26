fdbb740a7839f79a805aea41a00fde34c76e1029
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-26 15:33:02 -0300

    refactor(flutter): move all logos to `FacioSvgImages`
    BREAKING CHANGE: all logo SVG images were moved to `FacioSvgImages`. `logoPrimary` was renamed to `logoDark`. `logoSecondary` was renamed to `logoLight`. Please find and replace it in your application.

---
+++ packages/flutter/lib/src/generated/images.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_import
+
+import 'package:flutter_svg/svg.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class FacioSvgImages {
+  static final String packageName = 'facio_design_system';
+  /// Image For Logo
+  static final logo = SvgPicture.asset(
+    "assets/images/logo.svg",
+    package: packageName
+  );
+  /// Image For Logo 57 W
+  static final logo57w = SvgPicture.asset(
+    "assets/images/logo-57w.png",
+    package: packageName
+  );
+  /// Image For Logo Dark
+  static final logoDark = SvgPicture.asset(
+    "assets/images/logo-dark.svg",
+    package: packageName
+  );
+  /// Image For Logo Light
+  static final logoLight = SvgPicture.asset(
+    "assets/images/logo-light.svg",
+    package: packageName
+  );
+}
