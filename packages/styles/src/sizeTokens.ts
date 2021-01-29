d39d74521b5481313b645bf0a55da01819a0834d
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-29 00:09:19 -0300

    feat(styles): add `baseTripleHalf` and `fontInputLargeFontSize` size tokens

--- packages/styles/src/sizeTokens.ts
+++ packages/styles/src/sizeTokens.ts
@@ -9,9 +9,10 @@ export const baseSingleHalf = "12px"; // Commonly used for medium button vertica
 export const baseDouble = "16px"; // Twice the base value (8) = 16
 export const baseDoubleQuarter = "18px"; // New border radius for small buttons
 export const baseTriple = "24px"; // Commonly used for button label height
+export const baseTripleHalf = "28px"; // Used for medium input top padding
 export const baseQuadruple = "32px"; // Formerly used for small button height.
 export const baseQuadrupleHalf = "36px"; // After some experiments, this is the new small button size
-export const baseFivefold = "40px";
+export const baseFivefold = "40px"; // Used for large input height
 export const baseSixfold = "48px";
 export const baseSevenfold = "56px"; // Commonly used for app bar height
 export const baseEightfold = "64px"; // Commonly used for button min height
