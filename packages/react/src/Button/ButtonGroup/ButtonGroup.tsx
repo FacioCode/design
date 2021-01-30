ef254688db8228a79cc545e96f60124c8b2c947c
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-30 13:34:38 -0300

    fix(react): add missing support to size prop on ButtonGroup

--- packages/react/src/Button/ButtonGroup/ButtonGroup.tsx
+++ packages/react/src/Button/ButtonGroup/ButtonGroup.tsx
@@ -4,9 +4,11 @@ import { ButtonGroup as MaterialButtonGroup } from "@material-ui/core";
 
 type Render = (props: ButtonGroupProps, ref: ButtonGroupProps["ref"]) => JSX.Element;
 
-const render : Render = (props, ref) => (
-  <MaterialButtonGroup {...props} ref={ref} />
-);
+const render : Render = (props, ref) => {
+  const { size = "small", ...otherProps } = props;
+
+  return (<MaterialButtonGroup {...otherProps} ref={ref} size={size} />);
+};
 
 export const ButtonGroup = React.forwardRef<unknown, ButtonGroupProps>(render);
 ButtonGroup.displayName = "ButtonGroup";
