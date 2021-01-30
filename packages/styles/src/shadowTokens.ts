92b09ce63a8a9ebf1c1c22866ac54b19c78015b2
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-30 13:33:11 -0300

    feat(styles): add new shadow tokens for error

--- packages/styles/src/shadowTokens.ts
+++ packages/styles/src/shadowTokens.ts
@@ -2,5 +2,6 @@ export const elevation00 = "none";
 export const elevation02 = "0px 1px 3px 0px rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.24)"; // Elevation for contained button on hover state
 export const elevation04 = "0px 2px 4px 0px rgba(0, 0, 0, 0.2)"; // Elevation for snackbar content.
 export const elevation06 = "0px 2px 5px 0px rgba(0, 0, 0, 0.25)"; // Elevation for bottom sheet.
-export const focusVisibleButton = "0px 0px 0px 3px #53e2fe"; // Uses color.base.skyblue.40
+export const error = "0px 0px 0px 3px #fe538c"; // Uses color.base.indianred.40
+export const focusVisibleDefault = "0px 0px 0px 3px #53e2fe"; // Uses color.base.skyblue.40
 export const focusVisibleContainedButton = "0px 0px 0px 3px #3fbbe1"; // Uses color.base.skyblue.50
