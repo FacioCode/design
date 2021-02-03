bd0d11bb596861e8fec2d2f1cf2f55247cbeafdf
Jimmy Andrade <jimmy.andrade@facio.com>
2021-02-03 12:27:24 -0300

    refactor(react): remove `Facio` prefix from `Currency` class key

--- packages/react/src/Currency/Currency.styles.ts
+++ packages/react/src/Currency/Currency.styles.ts
@@ -5,6 +5,6 @@ import { customProperties } from "@faciocode/styles";
 const styles = createStyles({
   root: customProperties.currencyStyles.root as CSSProperties,
 });
-const useStyles = makeStyles(styles, { name: "FacioCurrency" });
+const useStyles = makeStyles(styles, { name: "Currency" });
 
 export default useStyles;
