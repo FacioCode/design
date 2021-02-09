5fae4774bc1482f07fd6be101fce3d8fe7a7b24f
Jimmy Andrade <jimmy.andrade@facio.com>
2021-02-09 17:40:15 -0300

    fix(flutter): prevent `FacioSvgImages` images for being coloured

--- packages/flutter/lib/components.dart
+++ packages/flutter/lib/components.dart
@@ -1,5 +1,5 @@
 export 'src/animations.dart';
 export 'src/components.dart';
-export 'src/generated/images.dart';
+export 'src/generated/svg_images.dart';
 export 'src/generated/svg_icons.dart';
 export 'styles.dart';
