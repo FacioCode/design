d9af30e947dd9a732a549d5b5f055ae82edd8efc
Jimmy Andrade <jimmy.andrade@facio.com>
2021-02-09 10:40:52 -0300

    ci: commit all changed dart formatter files

--- .github/scripts/commit-formatted-dart.sh
+++ .github/scripts/commit-formatted-dart.sh
@@ -4,11 +4,8 @@ set -e
 # Get git status for changes
 git status
 
-# Prepare changed files to be committed
-git add -a
-
 # Commit pubspec file
-git commit --allow-empty -m "style(flutter): format dart files with dartfmt [skip ci]"
+git commit -a --allow-empty -m "style(flutter): format dart files with dartfmt [skip ci]"
 
 # Push up-to-date pubspec file
 git push origin HEAD:main
