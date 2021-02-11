5a520c4d9aba9b65d77d055065ad542dc7ce5d4d
Jimmy Andrade <jimmy.andrade@facio.com>
2021-02-11 19:48:46 -0300

    feat(react): add `ScopedCssBaseline` component

---
+++ packages/react/src/ScopedCssBaseline/ScopedCssBaseline.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { default as MaterialScopedCssBaseline } from "@material-ui/core";
+
+export const ScopedCssBaseline = MaterialScopedCssBaseline;
+export default ScopedCssBaseline;
---
+++ packages/react/src/ScopedCssBaseline/index.ts
@@ -0,0 +1,4 @@
+import { ScopedCssBaseline } from "./ScopedCssBaseline";
+
+export { default as ScopedCssBaseline } from "./ScopedCssBaseline";
+export default ScopedCssBaseline;
