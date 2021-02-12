6548d9588fe40a7c873ef554c2675dc0d87551e9
Jimmy Andrade <jimmy.andrade@facio.com>
2021-02-12 20:05:55 -0300

    fix(react): change `ScopedCssBaseline` export strategy

--- packages/react/src/ScopedCssBaseline/ScopedCssBaseline.tsx
+++ packages/react/src/ScopedCssBaseline/ScopedCssBaseline.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-import { default as MaterialScopedCssBaseline } from "@material-ui/core/ScopedCssBaseline";
+import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
 
-export const ScopedCssBaseline = MaterialScopedCssBaseline;
-export default ScopedCssBaseline;
+export { ScopedCssBaseline };
