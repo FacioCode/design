a16ce0ae51b2d4eac7248b472e825df5a06848d6
Jimmy Andrade <jimmy.andrade@facio.com>
2021-02-12 20:09:30 -0300

    feat(mui-themes): add overrides for `MuiAlert`

--- packages/mui-themes/src/overrides/MuiAlert.ts
+++ packages/mui-themes/src/overrides/MuiAlert.ts
@@ -6,8 +6,18 @@ export const MuiAlert = {
   icon: customProperties.alertStyles.icon,
   message: customProperties.alertStyles.message,
   root: customProperties.alertStyles.root,
+  filledError: {
+    ...customProperties.alertStyles.filledError,
+    "& $icon": customProperties.alertStyles.filledErrorIcon,
+    "& $message": customProperties.alertStyles.filledErrorMessage,
+  },
   filledInfo: customProperties.alertStyles.filledInfo,
   filledWarning: customProperties.alertStyles.filledWarning,
+  standardError: {
+    ...customProperties.alertStyles.standardError,
+    "& $icon": customProperties.alertStyles.standardErrorIcon,
+    "& $message": customProperties.alertStyles.standardErrorMessage,
+  },
   standardInfo: customProperties.alertStyles.standardInfo,
   standardWarning: customProperties.alertStyles.standardWarning,
 };
