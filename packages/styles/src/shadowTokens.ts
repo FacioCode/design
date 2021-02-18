06b483d06b78e4aae1ab6e19bf405bfbdd385d64
Jimmy Andrade <jimmy.andrade@facio.com>
2021-02-18 19:01:29 -0300

    docs(styles): change comment for `elevation04`

--- packages/styles/src/shadowTokens.ts
+++ packages/styles/src/shadowTokens.ts
@@ -1,7 +1,7 @@
 export const elevation00 = "none";
 export const elevation01 = "0px 1px 3px 0px rgba(0, 0, 0, 0.2)"; // Elevation for switch thumb
 export const elevation02 = "0px 1px 3px 0px rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.24)"; // Elevation for contained button on hover state
-export const elevation04 = "0px 2px 4px 0px rgba(0, 0, 0, 0.2)"; // Elevation for snackbar content.
+export const elevation04 = "0px 2px 4px 0px rgba(0, 0, 0, 0.2)"; // Elevation for snackbar content. TODO: use correct value.
 export const elevation06 = "0px 2px 5px 0px rgba(0, 0, 0, 0.25)"; // Elevation for bottom sheet.
 export const error = "0px 0px 0px 3px #fe538c"; // Uses color.base.indianred.40
 export const focusVisibleDefault = "0px 0px 0px 3px #53e2fe"; // Uses color.base.skyblue.40
