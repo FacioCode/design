d78327b6de418b8823cb75265f2b5f2408a99fbc
Jimmy Andrade <jimmy.andrade@facio.com>
2021-03-31 14:42:04 -0300

    refactor(react): rename stories for `LabeledCurrency` document

--- packages/react/src/LabeledCurrency/index.stories.tsx
+++ packages/react/src/LabeledCurrency/index.stories.tsx
@@ -13,20 +13,15 @@ export default {
 } as Meta;
 
 const Template : Story<LabeledCurrencyProps> = (props: LabeledCurrencyProps) => (
-  <LabeledCurrency
-    code={props.code}
-    {...props}
-  >
-    {props.children}
-  </LabeledCurrency>
+  <LabeledCurrency {...props} />
 );
 
 export const Playground : Story<LabeledCurrencyProps> = Template.bind({});
 Playground.args = {
   "aria-label": "O total a pagar será de R$ 100",
-  children: 100,
+  children: "Total a pagar",
   code: "BRL",
-  label: "Total a pagar",
+  value: 100,
 };
 
 export const AssertiveAlertExample : Story<LabeledCurrencyProps> = Template.bind({});
@@ -39,6 +34,8 @@ AssertiveAlertExample.args = {
 export const VerticalExample : Story<LabeledCurrencyProps> = Template.bind({});
 VerticalExample.args = {
   ...Playground.args,
-  currencyVariant: "headline2",
+  CurrencyTypographyProps: {
+    variant: "headline2",
+  },
   orientation: "vertical",
 };
