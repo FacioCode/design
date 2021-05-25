5be4ebc1876dd0ca1e5164dc304390cfc9e16063
Jimmy Andrade <jimmy.andrade@facio.com>
2021-05-25 16:20:26 -0300

    style: update babel config code style

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+const babelOptions = {
+  exclude: /node_modules/u,
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
     "@babel/preset-typescript",
   ],
 };
+
+module.exports = babelOptions;
