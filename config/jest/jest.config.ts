eabca1c66c9973f541cac0ce031aaae9fc06faec
Jimmy Andrade <jimmy.andrade@facio.com>
2021-05-25 16:23:00 -0300

    test(jest): update jest config file

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,12 +1,26 @@
-const ignorePatterns = [
+import type { Config } from "@jest/types";
+
+const modulePathIgnorePatterns = [
   "/.stryker-tmp/",
+  "/coverage/",
+  "/dist/",
+];
+
+const ignorePatterns = [
+  ...modulePathIgnorePatterns,
   "/node_modules/",
 ];
 
-export default {
+const config: Config.InitialOptions = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coveragePathIgnorePatterns: ignorePatterns,
+  errorOnDeprecated: true,
+  modulePathIgnorePatterns,
   testPathIgnorePatterns: ignorePatterns,
+  transformIgnorePatterns: ignorePatterns,
   verbose: true,
+  watchPathIgnorePatterns: ignorePatterns,
 };
+
+export default config;
