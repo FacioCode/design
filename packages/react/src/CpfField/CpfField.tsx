9829649fdf644001e8d0521d33106d184fd4d179
Jimmy Andrade <jimmy.andrade@facio.com>
2021-06-24 19:55:10 -0300

    feat(react): add support to CPF validation for `CpfField`

--- packages/react/src/CpfField/CpfField.tsx
+++ packages/react/src/CpfField/CpfField.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-magic-numbers */
 import * as PropTypes from "prop-types";
 import * as React from "react";
+import { TextField, TextFieldProps } from "@components/TextField";
 import { CpfFieldProps } from "@components/CpfField/CpfField.types";
 import { PhoneFieldProps } from "@components/PhoneField";
-import { TextField } from "@components/TextField";
 import { useKeyDown } from "@hooks/useKeyDown";
 
 const allowedKeyGroups = [
@@ -17,11 +18,60 @@ const allowedKeyGroups = [
   "tab",
 ];
 
+const validValueLength = 11;
+
+// eslint-disable-next-line max-statements
+const isValid = (rawValue: string) => {
+  const value = rawValue.replace(/[^\d]+/gu, "");
+
+  if (value === "" || value.length !== validValueLength) {
+    return false;
+  }
+
+  const originalSumValue = 0;
+  let sum = originalSumValue;
+  // eslint-disable-next-line init-declarations
+  let remainder: number;
+
+  const firstDigitIndex = 0;
+  const nextDigitIncrement = 1;
+  const penultimateDigitIndex = 9;
+  const lastDigitIndex = 10;
+  const radix = 10;
+
+  // eslint-disable-next-line id-length
+  for (let i = firstDigitIndex; i < penultimateDigitIndex; i += nextDigitIncrement) {
+    sum += parseInt(value.charAt(i), radix) * (10 - i);
+  }
+
+  remainder = 11 - (sum % 11);
+  if ([10, 11].includes(remainder)) {
+    remainder = originalSumValue;
+  }
+  if (remainder !== parseInt(value.charAt(penultimateDigitIndex), radix)) {
+    return false;
+  }
+
+  sum = originalSumValue;
+
+  // eslint-disable-next-line id-length
+  for (let i = firstDigitIndex; i < lastDigitIndex; i += nextDigitIncrement) {
+    sum += parseInt(value.charAt(i), radix) * (11 - i);
+  }
+
+  remainder = 11 - (sum % 11);
+  if ([10, 11].includes(remainder)) {
+    remainder = originalSumValue;
+  }
+
+  return remainder === parseInt(value.charAt(lastDigitIndex), radix);
+};
+
 export const CpfField = React.forwardRef<unknown, CpfFieldProps>(
   (props, ref: PhoneFieldProps["ref"]) => {
     const defaultInputProps : CpfFieldProps["inputProps"] = {
       inputMode: "numeric",
-      minLength: 11,
+      minLength: validValueLength,
       pattern: "\\d{3}(\\s|.|-)*\\d{3}(\\s|.|-)*\\d{3}(\\s|.|-)*\\d{2}",
     };
 
@@ -37,6 +87,18 @@ export const CpfField = React.forwardRef<unknown, CpfFieldProps>(
       allowedKeyGroups,
     });
 
+    const handleChange : TextFieldProps["onChange"] = (event) => {
+      const { target } = event;
+      const { value } = target;
+
+      if (isValid(value)) {
+        target.setCustomValidity("");
+      }
+      if (!isValid(value)) {
+        target.setCustomValidity("Por favor, confira se o seu CPF foi digitado corretamente.");
+      }
+    };
+
     return (
       <TextField
         allowRecording={allowRecording}
@@ -45,6 +107,7 @@ export const CpfField = React.forwardRef<unknown, CpfFieldProps>(
           ...defaultInputProps,
           ...inputProps,
         }}
+        onChange={handleChange}
         onKeyDown={handleKeyDown}
         ref={ref}
         type={"text"}
