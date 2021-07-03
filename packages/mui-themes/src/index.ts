cdf94625b6e45f94f7f9d007d9d2551f34f11474
Jimmy Andrade <jimmy.andrade@facio.com>
2021-07-03 13:59:03 -0300

    feat(mui-themes): add new (unstable) `strictTheme` for strict mode
    See https://stackoverflow.com/questions/61220424/material-ui-drawer-finddomnode-is-deprecated-in-strictmode for more info

--- packages/mui-themes/src/index.ts
+++ packages/mui-themes/src/index.ts
@@ -1,4 +1,9 @@
-import { Theme, ThemeOptions, createMuiTheme } from "@material-ui/core";
+import {
+  Theme,
+  ThemeOptions,
+  unstable_createMuiStrictModeTheme as createMuiStrictModeTheme,
+  createMuiTheme,
+} from "@material-ui/core";
 import { MuiAlert } from "./overrides/MuiAlert";
 import { customProperties } from "@faciocode/styles";
 import { overrides } from "./overrides";
@@ -33,4 +38,9 @@ const createTheme: CreateTheme = (options: ThemeOptions) => createMuiTheme({
   ...options,
 });
 
+const createStrictTheme = (options: ThemeOptions) => createMuiStrictModeTheme({
+  ...options,
+});
+
+export const strictTheme = createStrictTheme(themeOptions);
 export const theme = createTheme(themeOptions);
