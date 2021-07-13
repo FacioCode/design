4acf9b22fe3d081a1b5da13c60e0731f38deed63
Jimmy Andrade <jimmy.andrade@facio.com>
2021-07-13 00:19:52 -0300

    fix(react): make `LabeledCurrency` props optional

--- packages/react/src/LabeledCurrency/LabeledCurrency.types.ts
+++ packages/react/src/LabeledCurrency/LabeledCurrency.types.ts
@@ -1,6 +1,6 @@
 import { CurrencyProps } from "@components/Currency";
 import { LabeledItemProps } from "@components/LabeledItem";
 
-export interface LabeledCurrencyProps extends LabeledItemProps {
+export interface LabeledCurrencyProps extends Partial<LabeledItemProps> {
   CurrencyProps?: CurrencyProps
 }
