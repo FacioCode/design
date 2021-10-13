91b9cca4ab329e39e64b2439c30b8cb557cb7b14
Camila Sousa <camila.sousa@facio.com>
2021-10-13 17:13:42 -0300

    fix: wrong border radius in medium outlined button

--- packages/flutter_ds/lib/src/components/buttons/medium_outlined_button.dart
+++ packages/flutter_ds/lib/src/components/buttons/medium_outlined_button.dart
@@ -31,7 +31,7 @@ class MediumOutlinedButton extends StatelessWidget {
           minimumSize: Size(88, 36),
           padding: EdgeInsets.symmetric(horizontal: 16),
           shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(2)),
+            borderRadius: BorderRadius.all(Radius.circular(Sizes.baseSingle)),
           ),
         ),
         child: Text(
